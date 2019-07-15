d0a07271a2211f12a870188847f27c93fde1f1d9
Fanny Rottee <frottee@tea-ebook.com>
2019-07-15 14:04:42 +0200

    Start extracting records from input

--- day-4/part-1/main.go
+++ day-4/part-1/main.go
@@ -4,10 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
+	"time"
+	"unicode"
 )
 
+type record struct {
+	extractedTime time.Time
+	minutes       string
+	action        string
+}
+
 func main() {
-	file, err := os.Open("./input")
+	file, err := os.Open("./example-input")
 
 	if err != nil {
 		fmt.Println("Error : ", err)
@@ -21,6 +30,28 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Println("Line : ", line)
+		record := recordsFromLine(line)
+	}
+}
+
+func recordsFromLine(line string) *record {
+	f := func(c rune) bool {
+		return !unicode.IsNumber(c) && !unicode.IsLetter(c)
+	}
+
+	result := strings.FieldsFunc(line, f)
+
+	// use regex instead ?
+	// re := regexp.MustCompile("\\[(.*?)\\]")
+	// fmt.Println(re.FindAllString(line, -1))
+	extractedTime, _ := time.Parse(time.RFC3339, result[0]+"-"+result[1]+"-"+result[2]+"T"+result[3]+":"+result[4]+":00Z")
+
+	fmt.Println("Line : ", result)
+
+	return &record{
+		extractedTime: extractedTime,
+		minutes:       result[4],
+		// Guard # / asleep / up
+		action: result[6],
 	}
 }
